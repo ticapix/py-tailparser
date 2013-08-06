9011178df6121a49de2e4260d554437b8ea743ef
Pierre Gronlier <pierre.gronlier@gmail.com>
2013-08-06 17:48:36 +0300

    add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tail-parser',
+      version='1.0',
+      description='Tailing log parser',
+      author='Pierre Gronlier',
+      url='https://github.com/ticapix/py-tailparser',
+      packages=['tail_parser']
+     )
